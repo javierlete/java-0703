8e85d4aeca740b3ffafdb3091d37e296a789e277
Javier Lete <javier@eskura21.com>
2025-07-01 11:49:35 +0200

    Ejemplo de bucles y condicionales

--- bases/src/bases/ArrayEjemplo.java
+++ bases/src/bases/ArrayEjemplo.java
@@ -5,23 +5,23 @@ import java.util.Arrays;
 public class ArrayEjemplo {
 	public static void main(String[] args) {
 		int[] arr;
-		
+
 		arr = new int[2];
-		
+
 		arr[0] = 5;
 		arr[1] = 6;
 //		arr[2] = 7; // ArrayIndexOutOfBoundsException
-		
+
 		System.out.println(Arrays.toString(arr));
 		System.out.println(arr.length);
-		
+
 		var tablero = new char[8][8];
-		
+
 		tablero[0][0] = 't';
 		tablero[1][0] = 'p';
 		tablero[6][7] = 'P';
 		tablero[7][7] = 'T';
-		
+
 		System.out.println(Arrays.toString(tablero[0]));
 		System.out.println(Arrays.toString(tablero[1]));
 		System.out.println(Arrays.toString(tablero[2]));
@@ -30,5 +30,41 @@ public class ArrayEjemplo {
 		System.out.println(Arrays.toString(tablero[5]));
 		System.out.println(Arrays.toString(tablero[6]));
 		System.out.println(Arrays.toString(tablero[7]));
+
+		buclePrincipal: for (var fila = 0; fila < tablero.length; fila++) {
+			for (var columna = 0; columna < tablero[fila].length; columna++) {
+				if (tablero[fila][columna] == 0) {
+					System.out.print(". ");
+				} else {
+					System.out.print(tablero[fila][columna] + " ");
+				}
+				
+				if(tablero[fila][columna] == 'P') {
+					break buclePrincipal;
+				}	
+			}
+
+			System.out.println();
+		}
+		
+		System.out.println();
+		
+		boolean seguir = true;
+		
+		for (var fila = 0; fila < tablero.length && seguir; fila++) {
+			for (var columna = 0; columna < tablero[fila].length && seguir; columna++) {
+				if (tablero[fila][columna] == 0) {
+					System.out.print(". ");
+				} else {
+					System.out.print(tablero[fila][columna] + " ");
+				}
+				
+				if(tablero[fila][columna] == 'P') {
+					seguir = false;
+				}	
+			}
+
+			System.out.println();
+		}
 	}
 }
--- bases/src/bases/Colecciones.java
+++ bases/src/bases/Colecciones.java
@@ -7,35 +7,57 @@ import java.util.HashSet;
 public class Colecciones {
 	public static void main(String[] args) {
 		var al = new ArrayList<String>();
-		
+
 		al.add("Uno");
 		al.add("Trees");
 		al.add("Patata");
 		al.add("Cuatro");
 		al.add("Cuatro");
-		
+
 		al.add(1, "Dos");
 		al.set(2, "Tres");
 		al.remove(3);
-		
+
 		System.out.println(al);
-		
+
+		for (var e : al) {
+			System.out.println(e);
+		}
+
 		var s = new HashSet<String>();
-		
+
 		s.add("Uno");
 		s.add("Dos");
 		s.add("Tres");
 		s.add("Cuatro");
 		s.add("Dos");
-		
+
 		System.out.println(s);
-		
+
 		var dic = new HashMap<String, Integer>();
-		
+
 		dic.put("Uno", 1);
 		dic.put("Dos", 2);
 		dic.put("Tres", 3);
-		
+
 		System.out.println(dic.get("Dos"));
+
+		System.out.println("CLAVES");
+
+		for (var clave : dic.keySet()) {
+			System.out.println(clave);
+		}
+
+		System.out.println("VALORES");
+
+		for (var valor : dic.values()) {
+			System.out.println(valor);
+		}
+
+		System.out.println("PARES");
+
+		for (var par : dic.entrySet()) {
+			System.out.println(par.getKey() + ", " + par.getValue());
+		}
 	}
 }
