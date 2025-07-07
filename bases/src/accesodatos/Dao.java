1514f16bfe93086a799f9231c85d48e13e25e228
Javier Lete <javier@eskura21.com>
2025-07-07 11:14:11 +0200

    DAO genérico

---
+++ bases/src/accesodatos/Dao.java
@@ -0,0 +1,11 @@
+package accesodatos;
+
+import java.util.Optional;
+
+public interface Dao<T> {
+	Iterable<T> obtenerTodos();
+	Optional<T> obtenerPorId(Long id);
+	T insertar(T objeto);
+	T modificar(T objeto);
+	void borrar(Long id);
+}
