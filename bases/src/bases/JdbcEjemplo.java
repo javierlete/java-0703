2c2ccefd37b7a92ece67154dd0870e96f1068cb7
Javier Lete <javier@eskura21.com>
2025-07-07 11:16:59 +0200

    Ejemplo de JDBC

---
+++ bases/src/bases/JdbcEjemplo.java
@@ -0,0 +1,44 @@
+package bases;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcEjemplo {
+	public static void main(String[] args) {
+//		String url = "jdbc:mysql://localhost:3306/basededatos";
+		String url = "jdbc:sqlite:bdd/ejemplo.db";
+		String user = "";
+		String pass = "";
+
+		try (Connection con = DriverManager.getConnection(url, user, pass)) {
+			String nombre = "Otro nuevo";
+			String fechaNacimiento = "2003-01-02'); DROP TABLE personas; --";
+			
+			String sql = "INSERT INTO personas (nombre, fecha_nacimiento) VALUES (?, ?)";
+			
+			try (PreparedStatement pst = con.prepareStatement(sql)) {
+				pst.setString(1, nombre);
+				pst.setString(2, fechaNacimiento);
+				
+				int numeroRegistrosModificados = pst.executeUpdate();
+
+				System.out.println(numeroRegistrosModificados);
+
+			}
+
+			try (PreparedStatement pst = con.prepareStatement("SELECT * FROM personas");
+					ResultSet rs = pst.executeQuery()) {
+				while (rs.next()) {
+					System.out.printf("%5s %-10s %10s\n", rs.getString("id"), rs.getString("nombre"),
+							rs.getString("fecha_nacimiento"));
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
