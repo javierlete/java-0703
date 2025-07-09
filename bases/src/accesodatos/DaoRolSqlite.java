8e5f8846da879052e4281842357365707ec0c8c9
Javier Lete <javier@eskura21.com>
2025-07-09 08:54:37 +0200

    Implementamos el DaoRol para SQLite

---
+++ bases/src/accesodatos/DaoRolSqlite.java
@@ -0,0 +1,69 @@
+package accesodatos;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import bibliotecas.AccesoDatosException;
+import bibliotecas.BaseDeDatos;
+import pojos.Rol;
+
+public class DaoRolSqlite implements DaoRol {
+	private final BaseDeDatos bdd;
+
+	public DaoRolSqlite(String url) {
+		bdd = new BaseDeDatos(url, null, null);
+	}
+
+	@Override
+	public Iterable<Rol> obtenerTodos() {
+		return bdd.ejecutarSql("SELECT * FROM roles", DaoRolSqlite::filaAObjeto);
+	}
+
+	@Override
+	public Optional<Rol> obtenerPorId(Long id) {
+		return bdd.ejecutarSqlUno("SELECT * FROM roles WHERE id=?", DaoRolSqlite::filaAObjeto, id);
+	}
+
+	@Override
+	public Rol insertar(Rol rol) {
+		bdd.ejecutarSql("INSERT INTO roles (nombre, descripcion) VALUES (?,?)", objetoAFila(rol));
+		return rol;
+	}
+
+	@Override
+	public Rol modificar(Rol rol) {
+		bdd.ejecutarSql("UPDATE roles SET nombre=?, descripcion=? WHERE id=?", objetoAFila(rol));
+		return rol;
+	}
+
+	@Override
+	public void borrar(Long id) {
+		bdd.ejecutarSql("DELETE FROM roles WHERE id=?", id);
+	}
+
+	@Override
+	public Optional<Rol> buscarPorNombre(String nombre) {
+		return bdd.ejecutarSqlUno("SELECT * FROM roles WHERE nombre=?", DaoRolSqlite::filaAObjeto, nombre);
+	}
+
+	private static Rol filaAObjeto(ResultSet rs) {
+		try {
+			var id = rs.getLong("id");
+			var nombre = rs.getString("nombre");
+			var descripcion = rs.getString("descripcion");
+
+			return new Rol(id, nombre, descripcion);
+		} catch (SQLException e) {
+			throw new AccesoDatosException("Error en el mapeado", e);
+		}
+	}
+
+	private static Object[] objetoAFila(Rol rol) {
+		if (rol.getId() != null) {
+			return new Object[] { rol.getNombre(), rol.getDescripcion(), rol.getId() };
+		} else {
+			return new Object[] { rol.getNombre(), rol.getDescripcion() };
+		}
+	}
+}
