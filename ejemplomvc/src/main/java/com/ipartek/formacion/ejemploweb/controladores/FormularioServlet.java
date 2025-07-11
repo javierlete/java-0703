23a517c857bede1026cd7d1f50819bc1a6a249f9
Javier Lete <javier@eskura21.com>
2025-07-11 11:04:24 +0200

    MVC de inserción

---
+++ ejemplomvc/src/main/java/com/ipartek/formacion/ejemploweb/controladores/FormularioServlet.java
@@ -0,0 +1,48 @@
+package com.ipartek.formacion.ejemploweb.controladores;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import com.ipartek.formacion.ejemploweb.modelos.Producto;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/formulario")
+public class FormularioServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		Recoger información de la petición
+//		Convertir la información
+//		Crear objetos de modelo
+//		Ejecutar lógica de negocio
+//		Empaquetar modelo para la vista
+//		Saltar a la siguiente vista
+		request.getRequestDispatcher("/WEB-INF/vistas/formulario.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+//		Recoger información de la petición
+		String nombre = request.getParameter("nombre");
+		String sPrecio = request.getParameter("precio");
+		
+//		Convertir la información
+		BigDecimal precio = new BigDecimal(sPrecio);
+		
+//		Crear objetos de modelo
+		Producto producto = new Producto(null, nombre, precio);
+		
+//		Ejecutar lógica de negocio
+		System.out.println(producto);
+		
+//		Empaquetar modelo para la vista
+//		Saltar a la siguiente vista
+		response.sendRedirect("listado");
+	}
+}
