1e4dfdb2724daf2cbe292ed04e345c14d9910137
Javier Lete <javier@eskura21.com>
2025-07-16 11:33:22 +0200

    Visualización con Bootstrap en Frontend

--- ejemplomvc/src/main/webapp/js/frontend.js
+++ ejemplomvc/src/main/webapp/js/frontend.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const URL = 'http://localhost:8080/ejemplomvc/api/productos/';
-let ul;
+let listado;
 
 window.addEventListener('DOMContentLoaded', async () => {
-	ul = document.querySelector('ul');
+	listado = document.querySelector('#listado');
 
 	rellenarProductos();
 
@@ -13,8 +13,14 @@ window.addEventListener('DOMContentLoaded', async () => {
 
 async function insertarProducto(evento) {
 	evento.preventDefault();
+	evento.stopPropagation();
 
 	const form = document.forms[0];
+
+	if (!form.checkValidity()) {
+		form.classList.add('was-validated');
+		return;
+	}
 
 	const producto = { nombre: form.nombre.value, precio: +form.precio.value };
 
@@ -25,7 +31,7 @@ async function insertarProducto(evento) {
 			"Content-Type": "application/json"
 		}
 	});
-	
+
 	rellenarProductos();
 }
 
@@ -33,15 +39,31 @@ async function rellenarProductos() {
 	const respuesta = await fetch(URL);
 	const productos = await respuesta.json();
 
-	ul.innerHTML = '';
-	
+	listado.innerHTML = '';
+
 	productos.forEach(rellenarProducto);
 }
 
 function rellenarProducto(producto) {
-	const li = document.createElement('li');
+	const div = document.createElement('div');
 
-	li.innerHTML = `Nombre: ${producto.nombre}, Precio: ${producto.precio}`;
+	div.className = 'col';
 
-	ul.appendChild(li);
+	div.innerHTML = `
+		<div class="card h-100">
+			<img src="https://picsum.photos/400/300?${producto.id}" class="card-img-top" alt="...">
+			<div class="card-body">
+				<h5 class="card-title">${producto.nombre}</h5>
+				<p class="card-text">This is a wider card with supporting text
+					below as a natural lead-in to additional content. This content is
+					a little bit longer.</p>
+				<div><a href="detalle?id=${producto.id}">Ver detalle</a></div>
+			</div>
+			<div class="card-footer">
+				<small class="text-body-secondary">${producto.precio}</small>
+			</div>
+		</div>
+	`;
+
+	listado.appendChild(div);
 }
