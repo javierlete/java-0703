e0b9ee27fa6245d15986777c8c3e6abbdf60ee68
Javier Lete <javier@eskura21.com>
2025-07-17 10:31:02 +0200

    Jakarta Validation con la implementación de Hibernate Validator

--- ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/modelos/Usuario.java
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/modelos/Usuario.java
@@ -1,5 +1,7 @@
 package com.ipartek.formacion.ejemplobibliotecas.modelos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +13,15 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Usuario {
 	private Long id;
+	
+	@NotBlank
 	private String nombre;
+	
+	@NotBlank
+//	@Email
+	@Pattern(message = "debe ser una dirección de correo electrónico con formato correcto", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 	private String email;
+
+	@NotBlank
 	private String password;
 }
---
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/pruebas/ValidacionesPruebas.java
@@ -0,0 +1,35 @@
+package com.ipartek.formacion.ejemplobibliotecas.pruebas;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import com.ipartek.formacion.ejemplobibliotecas.modelos.Producto;
+import com.ipartek.formacion.ejemplobibliotecas.modelos.Usuario;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
+public class ValidacionesPruebas {
+	public static void main(String[] args) {
+		var producto = Producto.builder().nombre("Prueba").precio(new BigDecimal("1234")).build();
+
+		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+		Validator validator = factory.getValidator();
+
+		Set<ConstraintViolation<Producto>> violations = validator.validate(producto);
+
+		for (ConstraintViolation<Producto> violation : violations) {
+			System.out.printf("%s: %s\n", violation.getPropertyPath(), violation.getMessage());
+		}
+
+		var usuario = Usuario.builder().nombre("Pepe").password("alkjdsfl").email("laksdjf@lkajd.com").build();
+
+		Set<ConstraintViolation<Usuario>> violationsUsuario = validator.validate(usuario);
+
+		for (ConstraintViolation<Usuario> violation : violationsUsuario) {
+			System.out.printf("%s: %s\n", violation.getPropertyPath(), violation.getMessage());
+		}
+	}
+}
