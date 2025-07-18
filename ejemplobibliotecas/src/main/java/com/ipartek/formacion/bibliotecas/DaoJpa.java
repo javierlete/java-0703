c3fe41523d16f582ded03d365d628f383ba6504e
Javier Lete <javier@eskura21.com>
2025-07-18 09:34:39 +0200

    Tercera refactorización del DAO de producto

--- ejemplobibliotecas/src/main/java/com/ipartek/formacion/bibliotecas/DaoJpa.java
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/bibliotecas/DaoJpa.java
@@ -8,46 +8,52 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 
-public class DaoJpa {
+public class DaoJpa<T> {
 	private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("com.ipartek.formacion.ejemplobibliotecas.modelos");
 	
-	public static <T> Iterable<T> obtenerTodos(Class<T> tipo) {
-		return operacionEntityManager(em -> em.createQuery("from Producto", tipo).getResultList());
+	private Class<T> tipo;
+	
+	public DaoJpa(Class<T> tipo) {
+		this.tipo = tipo;
+	}
+	
+	public Iterable<T> obtenerTodos() {
+		return operacionEntityManager(em -> em.createQuery("from " + tipo.getSimpleName(), tipo).getResultList());
 	}
 
-	public static <T> Optional<T> obtenerPorId(Class<T> tipo, Long id) {
+	public Optional<T> obtenerPorId(Long id) {
 		return operacionEntityManager(em -> Optional.ofNullable(em.find(tipo, id)));
 	}
 
-	public static <T> T insertar(T objeto) {
+	public T insertar(T objeto) {
 		return operacionEntityManager(em -> {
 			em.persist(objeto);
 			return objeto;
 		});
 	}
 
-	public static <T> T modificar(T objeto) {
+	public T modificar(T objeto) {
 		return operacionEntityManager(em -> {
 			em.merge(objeto);
 			return objeto;
 		});
 	}
 
-	public static <T> void borrar(Class<T> tipo, Long id) {
+	public void borrar(Long id) {
 		operacionEntityManager(em -> {
 			em.remove(em.find(tipo, id));
 			return null;
 		});
 	}
 
-	public static <T> T operacionEntityManager(Function<EntityManager, T> funcion) {
+	public <R> R operacionEntityManager(Function<EntityManager, R> funcion) {
 		EntityManager em = EMF.createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
 		
 		try {
 			transaction.begin();
 			
-			T resultado = funcion.apply(em);
+			R resultado = funcion.apply(em);
 			
 			transaction.commit();
 			
--- ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/accesodatos/DaoProductoJpa.java
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/accesodatos/DaoProductoJpa.java
@@ -1,34 +1,12 @@
 package com.ipartek.formacion.ejemplobibliotecas.accesodatos;
 
-import java.util.Optional;
-
 import com.ipartek.formacion.bibliotecas.DaoJpa;
 import com.ipartek.formacion.ejemplobibliotecas.entidades.Producto;
 
-public class DaoProductoJpa implements DaoProducto {
+public class DaoProductoJpa extends DaoJpa<Producto> implements DaoProducto {
+
+	public DaoProductoJpa(Class<Producto> tipo) {
+		super(tipo);
+	}
 	
-	@Override
-	public Iterable<Producto> obtenerTodos() {
-		return DaoJpa.obtenerTodos(Producto.class);
-	}
-
-	@Override
-	public Optional<Producto> obtenerPorId(Long id) {
-		return DaoJpa.obtenerPorId(Producto.class, id);
-	}
-
-	@Override
-	public Producto insertar(Producto producto) {
-		return DaoJpa.insertar(producto);
-	}
-
-	@Override
-	public Producto modificar(Producto producto) {
-		return DaoJpa.modificar(producto);
-	}
-
-	@Override
-	public void borrar(Long id) {
-		DaoJpa.borrar(Producto.class, id);
-	}
 }
