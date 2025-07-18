d8f268f7b1776a2c1b37421dc22c602abe56ab79
Javier Lete <javier@eskura21.com>
2025-07-18 09:16:33 +0200

    Segunda refactorización de DAO

--- ejemplobibliotecas/src/main/java/com/ipartek/formacion/bibliotecas/DaoJpa.java
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/bibliotecas/DaoJpa.java
@@ -1,5 +1,6 @@
 package com.ipartek.formacion.bibliotecas;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import jakarta.persistence.EntityManager;
@@ -10,6 +11,35 @@ import jakarta.persistence.Persistence;
 public class DaoJpa {
 	private static final EntityManagerFactory EMF = Persistence.createEntityManagerFactory("com.ipartek.formacion.ejemplobibliotecas.modelos");
 	
+	public static <T> Iterable<T> obtenerTodos(Class<T> tipo) {
+		return operacionEntityManager(em -> em.createQuery("from Producto", tipo).getResultList());
+	}
+
+	public static <T> Optional<T> obtenerPorId(Class<T> tipo, Long id) {
+		return operacionEntityManager(em -> Optional.ofNullable(em.find(tipo, id)));
+	}
+
+	public static <T> T insertar(T objeto) {
+		return operacionEntityManager(em -> {
+			em.persist(objeto);
+			return objeto;
+		});
+	}
+
+	public static <T> T modificar(T objeto) {
+		return operacionEntityManager(em -> {
+			em.merge(objeto);
+			return objeto;
+		});
+	}
+
+	public static <T> void borrar(Class<T> tipo, Long id) {
+		operacionEntityManager(em -> {
+			em.remove(em.find(tipo, id));
+			return null;
+		});
+	}
+
 	public static <T> T operacionEntityManager(Function<EntityManager, T> funcion) {
 		EntityManager em = EMF.createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
--- ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/accesodatos/DaoProductoJpa.java
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/accesodatos/DaoProductoJpa.java
@@ -1,44 +1,34 @@
 package com.ipartek.formacion.ejemplobibliotecas.accesodatos;
 
-import static com.ipartek.formacion.bibliotecas.DaoJpa.operacionEntityManager;
-
 import java.util.Optional;
 
+import com.ipartek.formacion.bibliotecas.DaoJpa;
 import com.ipartek.formacion.ejemplobibliotecas.entidades.Producto;
 
 public class DaoProductoJpa implements DaoProducto {
 	
 	@Override
 	public Iterable<Producto> obtenerTodos() {
-		return operacionEntityManager(em -> em.createQuery("from Producto", Producto.class).getResultList());
+		return DaoJpa.obtenerTodos(Producto.class);
 	}
 
 	@Override
 	public Optional<Producto> obtenerPorId(Long id) {
-		return operacionEntityManager(em -> Optional.ofNullable(em.find(Producto.class, id)));
+		return DaoJpa.obtenerPorId(Producto.class, id);
 	}
 
 	@Override
 	public Producto insertar(Producto producto) {
-		return operacionEntityManager(em -> {
-			em.persist(producto);
-			return producto;
-		});
+		return DaoJpa.insertar(producto);
 	}
 
 	@Override
 	public Producto modificar(Producto producto) {
-		return operacionEntityManager(em -> {
-			em.merge(producto);
-			return producto;
-		});
+		return DaoJpa.modificar(producto);
 	}
 
 	@Override
 	public void borrar(Long id) {
-		operacionEntityManager(em -> {
-			em.remove(em.find(Producto.class, id));
-			return null;
-		});
+		DaoJpa.borrar(Producto.class, id);
 	}
 }
