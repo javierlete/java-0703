aacc860155b4a641d5c2a6769eec90730fdf5403
Javier Lete <javier@eskura21.com>
2025-07-21 11:29:46 +0200

    Ejemplo de login que además distingue métodos y usa sesión

---
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/controladores/LoginControlador.java
@@ -0,0 +1,38 @@
+package com.ipartek.formacion.ejemplobibliotecas.controladores;
+
+import java.util.Map;
+import java.util.Optional;
+
+import com.ipartek.formacion.bibliotecas.Controlador;
+import com.ipartek.formacion.bibliotecas.Fabrica;
+import com.ipartek.formacion.ejemplobibliotecas.entidades.Usuario;
+import com.ipartek.formacion.ejemplobibliotecas.logicanegocio.AnonimoNegocio;
+
+public class LoginControlador implements Controlador {
+
+	@Override
+	public String ejecutar(String metodo, Map<String, String[]> mapaEntrada, Map<String, Object> mapaSalida,
+			Map<String, Object> sesionEntrada, Map<String, Object> sesionSalida) {
+
+		if("GET".equals(metodo)) {
+			return "/login";
+		}
+		
+		String email = mapaEntrada.get("email")[0];
+		String password = mapaEntrada.get("password")[0];
+
+		Usuario usuario = Usuario.builder().email(email).password(password).build();
+
+		AnonimoNegocio negocio = (AnonimoNegocio) Fabrica.obtener("negocio.anonimo");
+
+		Optional<Usuario> usuarioAutenticado = negocio.autenticar(usuario);
+
+		if (usuarioAutenticado.isEmpty()) {
+			return "redirect:/login";
+		} else {
+			sesionSalida.put("usuario", usuarioAutenticado.get());
+			return "redirect:/";
+		}
+	}
+
+}
---
+++ ejemplobibliotecas/src/main/webapp/WEB-INF/vistas/login.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="jakarta.tags.core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form method="post">
+		<input name="email" placeholder="Correo electrónico">
+		<input name="password" placeholder="Contraseña">
+		
+		<button>Inicio sesión</button>	
+	</form>
+	
+</body>
+</html>
