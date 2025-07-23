905f1d0c6489b4fa5de5deb576c0e7e6b0938efb
Javier Lete <javier@eskura21.com>
2025-07-23 11:13:06 +0200

    Relación bidireccional Producto-Categoria con exclusión de productos

--- ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/entidades/Categoria.java
+++ ejemplobibliotecas/src/main/java/com/ipartek/formacion/ejemplobibliotecas/entidades/Categoria.java
@@ -1,17 +1,23 @@
 package com.ipartek.formacion.ejemplobibliotecas.entidades;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
@@ -32,4 +38,9 @@ public class Categoria {
 	@Lob
 	@Size(max = 2000)
 	private String descripcion;
+	
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	@OneToMany(mappedBy = "categoria", fetch = FetchType.EAGER) // TODO Corregir esta mala práctica. Es mejor usar LAZY
+	private List<Producto> productos;
 }
