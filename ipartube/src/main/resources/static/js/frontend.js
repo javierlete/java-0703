a356307294458917f1c132cb74b150ff08549b6c
Desarrollo <Desarrollo@192.168.0.100>
2025-09-03 10:26:03 +0200

    Versión Frontend

---
+++ ipartube/src/main/resources/static/js/frontend.js
@@ -0,0 +1,52 @@
+'use strict';
+
+let ul, iframe;
+
+window.addEventListener('DOMContentLoaded', () => {
+	ul = document.querySelector('ul');
+	iframe = document.querySelector('iframe');
+
+	principal();
+
+	console.log(ul);
+	console.log(iframe);
+});
+
+async function principal() {
+	iframe.style.display = 'none';
+
+	const respuesta = await fetch('/api/videos');
+	const videos = await respuesta.json();
+
+	console.log(videos);
+
+	ul.innerHTML = '';
+	
+	videos.forEach(video => {
+		const li = document.createElement('li');
+
+		li.innerHTML = `
+				<a href="javascript:detalle(${video.id})">${video.titulo}</a>
+				(${video.usuario.nombre})
+			`;
+
+		ul.appendChild(li);
+	});
+
+	ul.style.display = null;
+
+	delete iframe.src;
+}
+
+async function detalle(id) {
+	ul.style.display = 'none';
+
+	const respuesta = await fetch('/api/videos/' + id);
+	const video = await respuesta.json();
+
+	console.log(video);
+
+	iframe.src = video.url;
+
+	iframe.style.display = null;
+}
