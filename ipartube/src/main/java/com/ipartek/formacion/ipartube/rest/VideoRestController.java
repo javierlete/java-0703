26c7997dd610c94bcd701ea501d0f647b0a21f62
Desarrollo <Desarrollo@192.168.0.100>
2025-09-10 10:31:06 +0200

    Frontend: videos paginados

--- ipartube/src/main/java/com/ipartek/formacion/ipartube/rest/VideoRestController.java
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/rest/VideoRestController.java
@@ -2,15 +2,12 @@ package com.ipartek.formacion.ipartube.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.formacion.ipartube.dtos.ComentarioDto;
@@ -24,10 +21,8 @@ public class VideoRestController {
 	private AnonimoService anonimoService;
 
 	@GetMapping
-	public Page<Video> get(@RequestParam(defaultValue = "0") int pagina,
-			@RequestParam(defaultValue = "3") int tamanoPagina, @RequestParam(defaultValue = "titulo") String ordenar,
-			@RequestParam(defaultValue = "ASC") Direction sentido) {
-		return anonimoService.verListadoVideos(PageRequest.of(pagina, tamanoPagina, Sort.by(sentido, ordenar)));
+	public Page<Video> get(@PageableDefault(page=0, size = 3, direction = Direction.ASC, sort = "titulo") Pageable pageable) {
+		return anonimoService.verListadoVideos(pageable);
 	}
 
 	@GetMapping("{id}")
--- ipartube/src/main/resources/static/js/frontend.js
+++ ipartube/src/main/resources/static/js/frontend.js
@@ -2,28 +2,28 @@
 
 let ul, iframe;
 
+const URL = '/api/videos';
+
 window.addEventListener('DOMContentLoaded', () => {
 	ul = document.querySelector('ul');
 	iframe = document.querySelector('iframe');
 
-	principal();
 
-	console.log(ul);
-	console.log(iframe);
+	principal();
 });
 
-async function principal() {
-	iframe.style.display = 'none';
+async function principal(pagina) {
+	mostrar('listado');
 
-	const respuesta = await fetch('/api/videos');
+	const respuesta = await fetch(`${URL}?page=${pagina}`);
 	const objeto = await respuesta.json();
-	
+
 	const videos = objeto.content;
 
 	console.log(videos);
 
 	ul.innerHTML = '';
-	
+
 	videos.forEach(video => {
 		const li = document.createElement('li');
 
@@ -35,20 +35,77 @@ async function principal() {
 		ul.appendChild(li);
 	});
 
-	ul.style.display = null;
-
 	delete iframe.src;
+
+	const page = objeto.page;
+
+	console.log(page);
+
+	document.querySelectorAll('.pagina').forEach(pagina => pagina.remove());
+	
+	for (let numeroPagina = page.totalPages - 1; numeroPagina >= 0; numeroPagina--) {
+		var li = document.createElement('li');
+		var a = document.createElement('a');
+
+		li.className = 'pagina';
+
+		li.appendChild(a);
+
+		
+		a.innerText = numeroPagina + 1;
+		
+		a.href = '#';
+		
+		a.addEventListener('click', e => {
+			e.preventDefault();
+			
+			principal(numeroPagina);
+		});
+
+		document.getElementById('anterior').parentElement.insertAdjacentElement("afterend", li);
+
+	}
+
+
+	document.getElementById('primero').onclick = e => {
+		e.preventDefault();
+
+		principal(0);
+	};
+
+	document.getElementById('anterior').onclick = e => {
+		e.preventDefault();
+
+		principal(page.number - 1);
+	};
+
+	document.getElementById('siguiente').onclick = e => {
+		e.preventDefault();
+
+		principal(page.number + 1);
+	};
+
+	document.getElementById('ultimo').onclick = e => {
+		e.preventDefault();
+
+		principal(page.totalPages - 1);
+	};
 }
 
 async function detalle(id) {
-	ul.style.display = 'none';
-
-	const respuesta = await fetch('/api/videos/' + id);
+	const respuesta = await fetch(`${URL}/${id}`);
 	const video = await respuesta.json();
 
 	console.log(video);
 
 	iframe.src = video.url;
 
-	iframe.style.display = null;
+	mostrar('detalle');
+}
+
+function mostrar(id) {
+	var sections = document.querySelectorAll('section');
+	sections.forEach(section => section.style.display = 'none');
+
+	document.getElementById(id).style.display = null;
 }
