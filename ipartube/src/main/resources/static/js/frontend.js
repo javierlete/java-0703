098aa395d590f724559598438786efbfae066a68
Desarrollo <Desarrollo@192.168.0.100>
2025-09-12 11:04:18 +0200

    Frontend mantenimiento de videos con listado (sin paginación) y editar

--- ipartube/src/main/resources/static/js/frontend.js
+++ ipartube/src/main/resources/static/js/frontend.js
@@ -1,13 +1,20 @@
 'use strict';
 
-let ul, iframe;
+let ul, iframe, tbody, formularioVideo;
 let tituloElemento, descripcionElemento, usuarioElemento, comentariosElemento, mostrarMasComentarios;
 
-const URL = '/api/videos';
+// TODO cambiar el ID por un usuario real
+const ID_USUARIO = 1;
+
+const URL = '/api';
+const URL_VIDEOS = URL + '/videos';
+const URL_USUARIOS = URL + '/usuarios';
 
 window.addEventListener('DOMContentLoaded', () => {
 	ul = document.querySelector('ul');
 	iframe = document.querySelector('iframe');
+	tbody = document.querySelector('tbody');
+	formularioVideo = document.querySelector('#mantenimiento form');
 
 	tituloElemento = document.getElementById('titulo');
 	descripcionElemento = document.getElementById('descripcion');
@@ -21,7 +28,7 @@ window.addEventListener('DOMContentLoaded', () => {
 async function principal(pagina) {
 	mostrar('listado');
 
-	const respuesta = await fetch(`${URL}?page=${pagina}`);
+	const respuesta = await fetch(`${URL_VIDEOS}?page=${pagina}`);
 	const objeto = await respuesta.json();
 
 	const videos = objeto.content;
@@ -48,7 +55,7 @@ async function principal(pagina) {
 	console.log(page);
 
 	document.querySelectorAll('.pagina').forEach(pagina => pagina.remove());
-	
+
 	for (let numeroPagina = page.totalPages - 1; numeroPagina >= 0; numeroPagina--) {
 		var li = document.createElement('li');
 		var a = document.createElement('a');
@@ -57,14 +64,14 @@ async function principal(pagina) {
 
 		li.appendChild(a);
 
-		
+
 		a.innerText = numeroPagina + 1;
-		
+
 		a.href = '#';
-		
+
 		a.addEventListener('click', e => {
 			e.preventDefault();
-			
+
 			principal(numeroPagina);
 		});
 
@@ -99,7 +106,7 @@ async function principal(pagina) {
 }
 
 async function detalle(id) {
-	const respuesta = await fetch(`${URL}/${id}`);
+	const respuesta = await fetch(`${URL_VIDEOS}/${id}`);
 	const video = await respuesta.json();
 
 	console.log(video);
@@ -107,18 +114,18 @@ async function detalle(id) {
 	tituloElemento.innerText = video.titulo;
 	descripcionElemento.innerText = video.descripcion;
 	usuarioElemento.innerText = video.usuario.nombre;
-	
+
 	iframe.src = video.url;
 
 	mostrar('detalle');
-	
+
 	comentariosElemento.innerHTML = '';
-	
+
 	await mostrarComentarios(id);
 }
 
 async function mostrarComentarios(id, pagina = 0) {
-	const respuestaComentarios = await fetch(`${URL}/${id}/comentarios?page=${pagina}&size=1`);
+	const respuestaComentarios = await fetch(`${URL_VIDEOS}/${id}/comentarios?page=${pagina}&size=1`);
 	const objeto = await respuestaComentarios.json();
 
 	const page = objeto.page;
@@ -137,12 +144,57 @@ async function mostrarComentarios(id, pagina = 0) {
 		comentariosElemento.appendChild(dt);
 		comentariosElemento.appendChild(dd);
 	});
-	
+
 	mostrarMasComentarios.onclick = e => {
 		e.preventDefault();
-		
+
 		mostrarComentarios(id, pagina + 1);
 	};
+}
+
+async function mantenimiento() {
+	mostrar('mantenimiento');
+
+	const respuesta = await fetch(`${URL_USUARIOS}/${ID_USUARIO}/videos`);
+	const pagina = await respuesta.json();
+
+	console.log(pagina);
+
+	tbody.innerHTML = '';
+
+	pagina.content.forEach(video => {
+		const tr = document.createElement('tr');
+
+		tr.innerHTML = `
+			<th>${video.id}</th>
+			<td>${video.titulo}</td>
+			<td>${video.url}</td>
+			<td>
+				<a href="javascript:formulario(${video.id})">Editar</a>
+				<a href="javascript:borrar(${video.id})">Borrar</a>
+			</td>
+		`;
+
+		tbody.appendChild(tr);
+	});
+}
+
+async function formulario(id) {
+	if (id) {
+		const respuesta = await fetch(`${URL_VIDEOS}/${id}`);
+		const video = await respuesta.json();
+		
+		formularioVideo['id'].value = video.id;
+		formularioVideo.titulo.value = video.titulo;
+		formularioVideo.url.value = video.url;
+		formularioVideo.descripcion.value = video.descripcion;
+	}
+}
+
+async function borrar(id) {
+	await fetch(`${URL_USUARIOS}/${ID_USUARIO}/videos/${id}`, { method: 'DELETE' });
+
+	mantenimiento();
 }
 
 function mostrar(id) {
